5d4bd67e11bd1e2dd82ad509e214f206913cee31
thrwwy216 <thrwwy216@tutanota.com>
2021-01-01 10:30:35 -0600

    init commit

---
+++ src/test/java/com/kwgdev/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwgdev.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
