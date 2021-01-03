28ef59c84673e8302435646ab433cb74fbbbd0cb
thrwwy216 <thrwwy216@tutanota.com>
2021-01-03 12:26:24 -0600

    run docker image of activeMQ server with Spring Boot JMS messaging app

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Based on John Thompson's course - https://github.com/springframeworkguru/sfg-jms
 2. ActiveMQ - managed by Apache, donated by RedHat
 
 See [https://github.com/vromero/activemq-artemis-docker](https://github.com/vromero/activemq-artemis-docker) for ActiveMQ docker image implementation
+
+To run:
+
+run the Spring Boot application ensuring the embedded ActiveMQ server is **not** running
+in windows command line
+run
+docker run -it --rm -p 8161:8161 -p 61616:61616 vromero/activemq-artemis
+
+log in and view server/message details
+
+auto-refresh is on in the ActiveMQ management console, disable listener component in Spring app to keep it from
+refreshing every 2 seconds (as a new message is sent in the app every 2 seconds)
--- src/main/java/com/kwgdev/sfgjms/SfgJmsApplication.java
+++ src/main/java/com/kwgdev/sfgjms/SfgJmsApplication.java
@@ -1,8 +1,8 @@
 package com.kwgdev.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
+//import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+//import org.apache.activemq.artemis.core.server.ActiveMQServer;
+//import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,13 +20,13 @@ public class SfgJmsApplication {
         // normally you would run an ActiveMQ Server outside of a Spring Boot Application
         // but we are doing it here to start for simplicity
 
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0"));
-
-        server.start();
+//        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//                .setPersistenceEnabled(false)
+//                .setJournalDirectory("target/data/journal")
+//                .setSecurityEnabled(false)
+//                .addAcceptorConfiguration("invm", "vm://0"));
+//
+//        server.start();
 
 
         SpringApplication.run(SfgJmsApplication.class, args);
